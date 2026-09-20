1d9d56a00a12829d59f88ec3ade962d0a224c6ea
18thdimention <doyeonxxxx@gmail.com>
2026-09-20 16:45:35 -0500

    wrap

--- nix/config/nvim/ftplugin/tex.lua
+++ nix/config/nvim/ftplugin/tex.lua
@@ -1,2 +1,3 @@
+vim.opt.wrap = true
 vim.keymap.set("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true, buffer = true })
 vim.keymap.set("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true, buffer = true })
--- nix/config/nvim/lua/config/options.lua
+++ nix/config/nvim/lua/config/options.lua
@@ -4,19 +4,19 @@ vim.cmd([[hi @lsp.type.number gui=bold]])
 vim.cmd([[set completeopt+=menuone,noselect,popup]])
 
 vim.g.mapleader = " "
+vim.g.vimtex_view_method = "zathura"
 
 vim.opt.winborder = "rounded"
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.showtabline = 0
 vim.opt.signcolumn = "yes"
-vim.opt.wrap = true
+vim.opt.wrap = false
 vim.opt.ignorecase = true
 vim.opt.smartindent = true
 vim.opt.termguicolors = true
 vim.opt.undofile = true
 vim.opt.number = true
-vim.opt.wrap = false
 vim.wo.relativenumber = true
 vim.opt.clipboard = "unnamedplus"
 vim.opt.scrolloff = 999
